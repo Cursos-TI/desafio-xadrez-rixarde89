0bc1450175c1779369182d81c1f716eec96cd070
Ricardo Carvalho <rixarde.4@gmail.com>
2025-11-08 14:21:53 +0000

    Xadrez novato

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,23 @@
 
 
 int main() {
-   int
+    int b = 0;
+    
+    
+    // Mover a torre 5 casas para a direita 
+    for (int t = 0; t < 5; t++) {
+        printf("Torre se movendo para a,");
+        printf("Direita\n");  //Impime a direção do movimento da torre
+    }
+
+    while (b <= 5) {
+
+      printf("%d\n", b);
+      printf("Bispo se movendo na diagonal para ");
+      printf("cima, e a direita");
+      b++;
+    }
+    
 
     return 0;
 }
