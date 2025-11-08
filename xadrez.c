0119ac26856553095d4ebe7558a4a5950af35448
Ricardo Carvalho <rixarde.4@gmail.com>
2025-11-08 14:51:36 +0000

    Xadrez novato finalizado

--- xadrez.c
+++ xadrez.c
@@ -1,24 +1,39 @@
 #include <stdio.h>
-
+#include <stdlib.h> // Usado para o return 0
 
 int main() {
-    int b = 0;
-    
-    
-    // Mover a torre 5 casas para a direita 
+    int b = 1;
+    int r = 0;
+
+
+    // --- 1. Movimento da TORRE (usando 'for') ---
+    // Requisito: 5 casas para a direita
+
+    printf("--- Movimento da Torre (5 casas) ---\n");
     for (int t = 0; t < 5; t++) {
-        printf("Torre se movendo para a,");
-        printf("Direita\n");  //Impime a direção do movimento da torre
+        printf("Direita\n"); //Imprime a direção do movimento da torre
+      
     }
 
-    while (b <= 5) {
+    // --- 2. Movimento do BISPO (usando 'while') ---
+    // Requisito: 5 casas na diagonal para cima e à direita
 
-      printf("%d\n", b);
-      printf("Bispo se movendo na diagonal para ");
-      printf("cima, e a direita");
+    printf("\n--- Movimento do Bispo (5 casas) ---\n");
+    while (b < 6) {
+      printf("Cima, Direita\n");
       b++;
     }
     
+    printf("\n--- Movimento da Rainha (8 casas) ---\n");
+    // --- 3. Movimento da RAINHA (usando 'do-while') ---
+    // Requisito: 8 casas para a esquerda
+
+    do {
+        printf("Esquerda\n");
+        r++; // 3. Incrementamos o contador
+    } 
+    while (r < 8);
+
 
     return 0;
 }
